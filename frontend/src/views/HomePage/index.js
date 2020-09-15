f8cb8b80f2d3e5123bf130e53cf52817361a9634
3AIMAQ20 <3AIMAQ20@fundacaonokia.org>
2020-09-15 15:37:11 -0400

    Added Logo Image in HomePage

--- frontend/src/views/HomePage/index.js
+++ frontend/src/views/HomePage/index.js
@@ -1,9 +1,28 @@
 import React from "react";
+import Logo from "../../assets/images/Logomarca_CLIO.jpg";
 
-// import { Container } from './styles';
+import "./styles.css";
 
 function HomePage() {
-  return <h1>Home Page</h1>;
+  return (
+    <div className="home-page-container">
+      <div
+        className="header-user"
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          margin: 10,
+          alignItems: "center",
+        }}
+      >
+        <img src={Logo} alt="CLIO" height={100} />
+        <div>
+          <h1>Ian Marcony</h1>
+          <h2>imarconyls@gmail.com</h2>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
